afdbc6b1b8ff02c99d2bcd82ab3ecee6cd335a46
Jingjing Duan <jingjing.duan@airbnb.com>
2017-09-07 23:40:35 -0700

    "with" expression

---
+++ with_scope.exs
@@ -0,0 +1,21 @@
+content = "Now is the time"
+
+lp = with {:ok, file} = File.open("/etc/passwd"),
+          content = IO.read(file, :all),
+          :ok = File.close(file),
+          [_, uid, gid] = Regex.run(~r/_lp:.*?:(\d+):(\d+)/, content)
+     do
+       "Group: #{gid}, User: #{uid}"
+     end
+
+IO.puts lp
+IO.puts content
+
+values = 1..10
+mean = with count = Enum.count(values),
+            sum = Enum.sum(values)
+       do
+         sum / count
+       end
+
+IO.puts "mean is #{mean}"
